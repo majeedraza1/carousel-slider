24b4129b2880d9fa91cfd5b56793db7284219578
Sayful Islam <sayful.islam001@gmail.com>
2021-09-22 18:40:09 +0600

    Update tooltip component.

--- resources/libs/tooltip/index.js
+++ resources/libs/tooltip/index.js
@@ -1,117 +1,175 @@
-class Tooltip {
-	constructor(element) {
-		this.element = element;
+import {createPopper} from '@popperjs/core'
 
-		this.cssClasses = {
-			IS_ACTIVE: 'is-active',
-			MAIN: 'shapla-tooltip',
-			BOTTOM: 'shapla-tooltip--bottom',
-			LEFT: 'shapla-tooltip--left',
-			RIGHT: 'shapla-tooltip--right',
-			TOP: 'shapla-tooltip--top'
-		};
+class Tooltip {
+
+	/**
+	 * Register automatically
+	 */
+	static register() {
+		let elements = document.querySelectorAll(".cs-tooltip, [data-tooltip-target], [data-tooltip]");
+		if (elements.length) {
+			elements.forEach(element => new Tooltip(element));
+		}
+	}
+
+	/**
+	 * @param {HTMLElement} element
+	 * @param {object} options
+	 */
+	constructor(element, options = {}) {
+		this.uuid = this.createUUID();
+		this.forElement = this.updateTooltipTargetElement(element);
+		this.element = null;
+		this.options = Object.assign({
+			theme: 'dark',
+			html: true,
+			container: 'body',
+			mainClass: 'shapla-tooltip',
+			activeClass: 'is-active',
+			removeOnClose: true
+		}, options);
+
+		this.popperInstance = null;
 
 		// Initialize instance.
 		this.init();
 	}
 
 	/**
-	 * Handle mouseenter for tooltip.
-	 *
-	 * @param {Event} event The event that fired.
-	 * @private
-	 */
-	handleMouseEnter_(event) {
-		let props = event.target.getBoundingClientRect();
-		let left = props.left + (props.width / 2);
-		let top = props.top + (props.height / 2);
-		let marginLeft = -1 * (this.element.offsetWidth / 2);
-		let marginTop = -1 * (this.element.offsetHeight / 2);
-
-		if (this.element.classList.contains(this.cssClasses.LEFT) || this.element.classList.contains(this.cssClasses.RIGHT)) {
-			if (top + marginTop < 0) {
-				this.element.style.top = '0';
-				this.element.style.marginTop = '0';
-			} else {
-				this.element.style.top = top + 'px';
-				this.element.style.marginTop = marginTop + 'px';
-			}
-		} else {
-			if (left + marginLeft < 0) {
-				this.element.style.left = '0';
-				this.element.style.marginLeft = '0';
-			} else {
-				this.element.style.left = left + 'px';
-				this.element.style.marginLeft = marginLeft + 'px';
-			}
-		}
-
-		if (this.element.classList.contains(this.cssClasses.TOP)) {
-			this.element.style.top = props.top - this.element.offsetHeight - 10 + 'px';
-		} else if (this.element.classList.contains(this.cssClasses.RIGHT)) {
-			this.element.style.left = props.left + props.width + 10 + 'px';
-		} else if (this.element.classList.contains(this.cssClasses.LEFT)) {
-			this.element.style.left = props.left - this.element.offsetWidth - 10 + 'px';
-		} else {
-			this.element.style.top = props.top + props.height + 10 + 'px';
-		}
-
-		this.element.classList.add(this.cssClasses.IS_ACTIVE);
-	};
-
-	/**
-	 * Hide tooltip on mouseleave or scroll
-	 *
-	 * @private
-	 */
-	hideTooltip_() {
-		this.element.classList.remove(this.cssClasses.IS_ACTIVE);
-	};
-
-	/**
 	 * Initialize element.
 	 */
 	init() {
+		const showEvents = ['mouseenter', 'focus'];
+		const hideEvents = ['mouseleave', 'blur'];
+
+		showEvents.forEach(event => {
+			this.forElement.addEventListener(event, () => this.show());
+		});
+
+		hideEvents.forEach(event => {
+			this.forElement.addEventListener(event, () => this.hide());
+		});
+	}
+
+	/**
+	 * Show tooltip
+	 */
+	show() {
+		this.createTooltipElementIfNotExists();
+
+		this.element.classList.add(this.options.activeClass);
+
+		this.popperInstance = createPopper(this.forElement, this.element, {
+			modifiers: [
+				{
+					name: 'offset',
+					options: {
+						offset: [0, 8],
+					},
+				},
+			],
+		});
+
+		// Enable the event listeners
+		this.popperInstance.setOptions((options) => ({
+			...options,
+			modifiers: [
+				...options.modifiers,
+				{name: 'eventListeners', enabled: true},
+			],
+		}));
+
+		this.popperInstance.update();
+	}
+
+	/**
+	 * Hide tooltip
+	 */
+	hide() {
+		this.element.classList.remove(this.options.activeClass);
+
+		// Disable the event listeners
+		if (this.popperInstance) {
+			this.popperInstance.setOptions((options) => ({
+				...options,
+				modifiers: [
+					...options.modifiers,
+					{name: 'eventListeners', enabled: false},
+				],
+			}));
+		}
+
+		if (this.options.removeOnClose) {
+			setTimeout(() => this.element.remove(), 10);
+		}
+	}
+
+	/**
+	 * Validate tooltip and tooltip for elements
+	 */
+	createTooltipElementIfNotExists() {
+		this.element = document.querySelector(`[data-tooltip-for="${this.uuid}"]`)
+
 		if (!this.element) {
-			return;
+			let content = this.forElement.getAttribute('data-tooltip') ||
+				this.forElement.getAttribute('title');
+
+			this.element = this.createTooltipElement(content);
 		}
+	}
 
-		let forElId = this.element.getAttribute('data-tooltip-for');
+	/**
+	 * Create tooltip element
+	 *
+	 * @param {string} content
+	 * @returns {HTMLDivElement}
+	 */
+	createTooltipElement(content) {
+		// Create arrow element, <div class="tooltip__arrow"></div>
+		let arrowElement = document.createElement("div");
+		arrowElement.setAttribute('data-popper-arrow', '');
+		arrowElement.classList.add(this.options.mainClass + '__arrow');
 
-		if (forElId) {
-			this.forElement_ = document.getElementById(forElId);
+		// Create arrow element, <div class="tooltip__inner"></div>
+		let innerElement = document.createElement("div");
+		innerElement.classList.add(this.options.mainClass + '__inner');
+		if (this.options.html) {
+			innerElement.innerHTML = content;
 		} else {
-			let content = this.element.getAttribute('data-tooltip') || this.element.getAttribute('title'),
-				uuid = Tooltip.createUUID();
-
-			let cElement = document.createElement("div");
-			cElement.classList.add(this.cssClasses.MAIN);
-			cElement.setAttribute('data-tooltip-for', uuid);
-			cElement.setAttribute('role', 'tooltip');
-			cElement.innerText = content;
-			document.body.appendChild(cElement);
-
-			this.forElement_ = this.element;
-			this.forElement_.setAttribute('data-tooltip-target', uuid);
-			this.forElement_.removeAttribute('data-tooltip');
-			this.forElement_.removeAttribute('title');
-			this.element = cElement;
+			innerElement.innerText = content;
 		}
 
-		if (this.forElement_) {
-			// It's left here because it prevents accidental text selection on Android
-			if (!this.forElement_.hasAttribute('tabindex')) {
-				this.forElement_.setAttribute('tabindex', '0');
-			}
+		// Create main element, <div class="tooltip"></div>
+		let mainElement = document.createElement("div");
+		mainElement.classList.add(this.options.mainClass);
+		mainElement.classList.add(`is-theme-${this.options.theme}`);
+		mainElement.setAttribute('data-tooltip-for', this.uuid);
+		mainElement.setAttribute('data-remove-on-close', '');
+		mainElement.setAttribute('role', 'tooltip');
+		mainElement.appendChild(arrowElement);
+		mainElement.appendChild(innerElement);
 
-			this.boundMouseEnterHandler = this.handleMouseEnter_.bind(this);
-			this.boundMouseLeaveAndScrollHandler = this.hideTooltip_.bind(this);
-			this.forElement_.addEventListener('mouseenter', this.boundMouseEnterHandler, false);
-			this.forElement_.addEventListener('touchend', this.boundMouseEnterHandler, false);
-			this.forElement_.addEventListener('mouseleave', this.boundMouseLeaveAndScrollHandler, false);
-			window.addEventListener('scroll', this.boundMouseLeaveAndScrollHandler, true);
-			window.addEventListener('touchstart', this.boundMouseLeaveAndScrollHandler);
+		let containerElement = document.querySelector(this.options.container);
+		containerElement.appendChild(mainElement);
+
+		return mainElement;
+	}
+
+	/**
+	 * Update tooltip for element
+	 *
+	 * @param {HTMLDivElement} targetElement
+	 * @returns {HTMLDivElement}
+	 */
+	updateTooltipTargetElement(targetElement) {
+		let _content = targetElement.getAttribute('data-tooltip') || targetElement.getAttribute('title');
+		targetElement.setAttribute('aria-describedby', 'tooltip');
+		targetElement.setAttribute('data-tooltip-target', this.uuid);
+		targetElement.setAttribute('data-tooltip', _content);
+		if (targetElement.hasAttribute('title')) {
+			targetElement.removeAttribute('title');
 		}
+		return targetElement
 	}
 
 	/**
@@ -119,23 +177,13 @@ class Tooltip {
 	 *
 	 * @returns {string}
 	 */
-	static createUUID() {
+	createUUID() {
 		const pattern = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
 		return pattern.replace(/[xy]/g, (c) => {
 			const r = (Math.random() * 16) | 0;
 			const v = c === 'x' ? r : ((r & 0x3) | 0x8);
 			return v.toString(16);
 		});
-	}
-
-	/**
-	 * Register automatically
-	 */
-	static register() {
-		let elements = document.querySelectorAll("[data-tooltip-for], [data-tooltip]");
-		if (elements.length) {
-			elements.forEach(element => new Tooltip(element));
-		}
 	}
 }
 
