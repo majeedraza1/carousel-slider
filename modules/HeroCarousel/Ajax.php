aceba5998dd297db237e53523962cde57b1502e0
Sayful Islam <sayful.islam001@gmail.com>
2021-12-04 19:08:28 +0600

    Update HeroCarousel Ajax class for better readability.

--- modules/HeroCarousel/Ajax.php
+++ modules/HeroCarousel/Ajax.php
@@ -30,35 +30,36 @@ class Ajax {
 			wp_send_json( __( 'Required attribute is not set properly.', 'carousel-slider' ), 422 );
 		}
 
-		$post_id   = isset( $_POST['post_id'] ) ? absint( $_POST['post_id'] ) : 0;
-		$task      = isset( $_POST['task'] ) ? sanitize_text_field( $_POST['task'] ) : 'add-slide';
-		$slide_pos = isset( $_POST['slide_pos'] ) && is_numeric( $_POST['slide_pos'] ) ? absint( $_POST['slide_pos'] ) : null;
+		$post_id = isset( $_POST['post_id'] ) ? absint( $_POST['post_id'] ) : 0;
+		$task    = isset( $_POST['task'] ) ? sanitize_text_field( $_POST['task'] ) : 'add-slide';
 
 		$slider_content = get_post_meta( $post_id, '_content_slider', true );
 		$slider_content = is_array( $slider_content ) ? $slider_content : [];
-		$last_index     = count( $slider_content ) - 1;
 
 		if ( $task == 'add-slide' ) {
-			$data             = [
-				'slide_heading'     => 'Slide Heading',
-				'slide_description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, magnam!',
-			];
-			$slider_content[] = wp_parse_args( $data, Item::get_default() );
+			$new_content = $this->add_new_item( $post_id, $slider_content );
+			wp_send_json( $new_content, 201 );
 		}
-		if ( $task == 'delete-slide' && ! is_null( $slide_pos ) ) {
-			array_splice( $slider_content, $slide_pos, 1 );
+
+		$last_index    = count( $slider_content ) - 1;
+		$current_index = $this->get_current_index( $last_index );
+		if ( $current_index === - 1 ) {
+			wp_send_json_error();
 		}
-		if ( $task == 'move-slide-top' && ! is_null( $slide_pos ) && $slide_pos !== 0 ) {
-			$slider_content = $this->move_array_element( $slider_content, $slide_pos, 0 );
+
+		$new_index = [
+			'move-slide-top'    => 0,
+			'move-slide-up'     => $current_index - 1,
+			'move-slide-down'   => $current_index + 1,
+			'move-slide-bottom' => $last_index,
+		];
+
+		if ( $task == 'delete-slide' ) {
+			array_splice( $slider_content, $current_index, 1 );
 		}
-		if ( $task == 'move-slide-up' && ! is_null( $slide_pos ) ) {
-			$slider_content = $this->move_array_element( $slider_content, $slide_pos, $slide_pos - 1 );
-		}
-		if ( $task == 'move-slide-down' && ! is_null( $slide_pos ) && $slide_pos !== $last_index ) {
-			$slider_content = $this->move_array_element( $slider_content, $slide_pos, ( $slide_pos + 1 ) );
-		}
-		if ( $task == 'move-slide-bottom' && ! is_null( $slide_pos ) && $slide_pos !== $last_index ) {
-			$slider_content = $this->move_array_element( $slider_content, $slide_pos, $last_index );
+
+		if ( array_key_exists( $task, $new_index ) ) {
+			$slider_content = $this->move_array_element( $slider_content, $current_index, $new_index[ $task ] );
 		}
 
 		update_post_meta( $post_id, '_content_slider', $slider_content );
@@ -79,5 +80,36 @@ class Ajax {
 		array_splice( $array, $new_index, 0, $output );
 
 		return $array;
+	}
+
+	/**
+	 * @param int $post_id
+	 * @param array $slider_content
+	 *
+	 * @return array
+	 */
+	protected function add_new_item( int $post_id, array $slider_content ): array {
+		$data             = [
+			'slide_heading'     => 'Slide Heading',
+			'slide_description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, magnam!',
+		];
+		$slider_content[] = wp_parse_args( $data, Item::get_default() );
+		update_post_meta( $post_id, '_content_slider', $slider_content );
+
+		return $slider_content;
+	}
+
+	/**
+	 * @param int $last_index
+	 *
+	 * @return int
+	 */
+	protected function get_current_index( int $last_index ): int {
+		$index = isset( $_POST['slide_pos'] ) && is_numeric( $_POST['slide_pos'] ) ? absint( $_POST['slide_pos'] ) : - 1;
+		if ( in_array( $index, range( 0, $last_index ), true ) ) {
+			return $index;
+		}
+
+		return - 1;
 	}
 }
